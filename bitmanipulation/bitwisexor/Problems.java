c91912e5ed0eee38a9630a1b3e9169022a87d1d0
S A Babu <babu.sa@bigcity.in>
2026-05-14 22:17:00 +0530

    solved single number 111 from leetcode

--- bitmanipulation/bitwisexor/Problems.java
+++ bitmanipulation/bitwisexor/Problems.java
@@ -2,17 +2,17 @@ package bitmanipulation.bitwisexor;
 
 public class Problems {
     public int singleNumber(int[] nums) {
-        int result =0;
-        for(int num : nums){
+        int result = 0;
+        for (int num : nums) {
             result ^= num;
         }
         return result;
     }
 
-    public void swap(int a,int b){
-        a = a^b;
-        b = a^b;
-        a = a^b;
+    public void swap(int a, int b) {
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
     }
 
     public int missingNumber(int[] nums) {
@@ -23,17 +23,36 @@ public class Problems {
         return xor;
     }
 
-    public int singleNumber(int[] nums) {
-        int one =0;
-        int two =0;
-        for(int num:nums){
+    public int singleNumber11(int[] nums) {
+        int one = 0;
+        int two = 0;
+        for (int num : nums) {
             one = (one ^ num) & ~two;
             two = (two ^ num) & ~one;
         }
         return one;
     }
 
-    public static void main(String[] args){
-        
+    public int[] singleNumber111(int[] nums) {
+        int xor = 0;
+        for (int num : nums) {
+            xor ^= num;
+        }
+
+        int rightMostBit = xor & (-xor);
+
+        int a = 0, b = 0;
+        for (int num : nums) {
+            if ((num & rightMostBit) == 0) {
+                a ^= num;
+            } else {
+                b ^= num;
+            }
+        }
+        return new int[] { a, b };
+    }
+
+    public static void main(String[] args) {
+
     }
 }
