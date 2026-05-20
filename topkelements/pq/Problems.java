f247b3bc7ffa62a37fab316cc27ed47ca48dc9a9
S A Babu <babu.sa@bigcity.in>
2026-05-20 15:19:45 +0530

    added kth smallest element in a sorted matrix using priority queue

--- topkelements/pq/Problems.java
+++ topkelements/pq/Problems.java
@@ -52,6 +52,26 @@ public class Problems {
         return result;
     }
 
+    public int kthSmallest(int[][] matrix, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        int n = matrix.length;
+        
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<n;j++){
+                if(pq.size() < k){
+                    pq.add(matrix[i][j]);
+                }else{ //equal to k
+                    if(matrix[i][j] < pq.peek()){ //if incoming element is less than peek
+                        pq.poll();
+                        pq.add(matrix[i][j]);
+                    }
+                }
+            }
+        }
+        return pq.peek();
+    }
+
     public static void main(String[] args) {
         int[] nums = {1,2,2,3,4,5};
         System.out.println(findKthLargest(nums,2));
