4e1263862aa15949e52cd0efcf97e137e521096c
S A Babu <babu.sa@bigcity.in>
2026-05-22 14:24:06 +0530

    solved find k pairs with smallest sums leetcode 373 porblem unsing priority Queue

--- topkelements/pq/Problems.java
+++ topkelements/pq/Problems.java
@@ -92,6 +92,28 @@ public class Problems {
     private int distance(int[] point){
         return point[0]*point[0] + point[1]*point[1];
     }
+    public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+        PriorityQueue<int[]>pq = new PriorityQueue<>((a,b)->
+        (a[0]+a[1])- (b[0]+b[1]));
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        for(int i=0;i<Math.min(nums1.length,k);i++){
+            pq.offer(new int[]{nums1[i],nums2[0],0});
+        }
+        while (k-->0 && !pq.isEmpty()) {
+            int[] pair = pq.poll();
+            int u= pair[0];
+            int v = pair[1];
+            int index =pair[2];
+            result.add(Arrays.asList(u,v));
+
+            if(index+1<nums2.length){
+                pq.offer(new int[]{u,nums2[index+1],index+1});
+            }
+        }
+        return result;
+    }
 
     public static void main(String[] args) {
         int[] nums = {1,2,2,3,4,5};
